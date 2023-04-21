75b58d8dc6f01ed00d3267a20dd0e25791fb859a
mike zylka <mikez@GoldenDragon.mynetworksettings.com>
2023-04-21 14:47:27 -0400

    Current implementation based on instructions. Migrating implementation for running on image load

--- source/cet_emulator.cpp
+++ source/cet_emulator.cpp
@@ -1,24 +1,64 @@
 #include "pin.H"
 #include <iostream>
+#include <vector>
 
-// This function will be called whenever a call instruction is encountered
-VOID CallInstrumentation(ADDRINT callInstructionAddress) {
-    std::cout << "Call Instruction: " << std::hex << callInstructionAddress << std:: endl; 
+// Global vector representing the shadow stack
+std::vector<ADDRINT> shadowStack;
+
+/* ===================================================================== */
+/* Trigger function whenever a call instruction is encountered           */
+/* ===================================================================== */
+VOID CallHandler(ADDRINT InstructionAddress) {
+    
 }
 
-// Intrumentation routine that will be called by Pin
+
+/* ===================================================================== */
+/* Trigger function whenever a RET instruction is encountered           */
+/* ===================================================================== */
+VOID RetHandler(ADDRINT retAddr, CONTEXT* ctxt) { 
+    
+}
+
+/* ===================================================================== */
+/* Intrumentation routine that will be called by Pin                     */
+/* ===================================================================== */
 VOID InstructionInstrumentation(INS ins, VOID *v) {
 
-    // Insert a call to CallInstrumentation before every call instruction
+    // Sets callback function for call instruction
     if (INS_IsCall(ins)) {
-        INS_InsertCall(ins, IPOINT_BEFORE, AFUNPTR(CallInstrumentation), IARG_INST_PTR, IARG_END);
+        INS_InsertCall(ins, IPOINT_BEFORE, AFUNPTR(CallHandler), IARG_INST_PTR, IARG_END);
+    }
+    // Sets callback function for ret instructions
+    else if (INS_IsRet(ins)) {
+        INS_InsertCall(ins, IPOINT_BEFORE, AFUNPTR(RetHandler), IARG_BRANCH_TARGET_ADDR, IARG_CONST_CONTEXT, IARG_END);
+    }
+
+    // Sets callback function for any indirect branch or call instruction
+    if (INS_IsIndirectControlFlow(ins)) {
+
     }
 }
 
+/* ===================================================================== */
+/* Prints out help message                                               */
+/* ===================================================================== */
+INT32 Usage() {
+    PIN_ERROR("A Control-flow enforcement emulator that runs a given executable with CET enabled, and detects violations of control-flow integrity.\n\
+    Usage: cet_emulator [OPTIONS] -- [PROGRAM] [ARGS]\n" + \
+    KNOB_BASE::StringKnobSummary() + "\n");
+    return -1;
+}
+
+
+/* ===================================================================== */
+/* Main                                                                  */
+/* ===================================================================== */
 int main(int argc, char *argv[]) {
     
     // Initialize Pin
-    PIN_Init(argc, argv);
+    if (PIN_Init(argc, argv)) return Usage();
+    PIN_InitSymbols();
 
     // Register InstructionInstrumentation to be called for every instruction
     INS_AddInstrumentFunction(InstructionInstrumentation, 0);
