a7f612e8a8c98ffb194df1e0e0f9f14ef1bd7433
mike zylka <mikez@KLM.mynetworksettings.com>
2023-04-27 22:37:56 -0400

    Finalized and properly documented IBT. Entering Testing stage.

--- source/cet_emulator.cpp
+++ source/cet_emulator.cpp
@@ -9,94 +9,109 @@
 
 #define ENDBR64 4196274163
 
-std::unordered_map<std::string, BOOL> COMPATIBLE_IMGS;
+// Data structure that will store the IDs of routines compiled with IBT
+std::unordered_map<ADDRINT, BOOL> COMPATIBLE_RTNS;
+
+// Shadow stack implemented as a stack of instruction pointer addresses
 std::stack<ADDRINT> SHADOW_STACK;
- 
-// Pin calls this function every time a new rtn is executed
-VOID Routine(RTN rtn, VOID* v)
-{
-    RTN_Open(rtn);
- 
-    UINT32* first_instr = (UINT32*) RTN_Address(rtn);
-    UINT32 instruction;
-    PIN_SafeCopy(&instruction, first_instr, sizeof(UINT32));
- 
-    
-    if (instruction == ENDBR64) {
-        
+
+
+/* ======================================================================== */
+/* Function called for every indirect branch instruction inside routines    */
+/* compiled with IBT.                                                       */
+/* ======================================================================== */
+VOID IBT_Instrumentation(ADDRINT* target_intr) {
+    // Determine routine from target address
+    PIN_LockClient();
+    RTN rtn = RTN_FindByAddress((ADDRINT) target_intr);
+    PIN_UnlockClient();
+
+    // Retrieve the routine ID from the routine
+    UINT32 rtn_id = RTN_Id(rtn);
+
+    // Check if the routine is compiled with IBT
+    if (COMPATIBLE_RTNS.count(rtn_id)) {
+        UINT32 instruction;
+        // Read the instruction at the branch target address
+        PIN_SafeCopy(&instruction, target_intr, sizeof(UINT32));
+
+        // Check if the instruction is ENDBR64
+        if (instruction != ENDBR64) {
+            std::cout << "Branch target address is not endbr64! Execution stopped."  << std::endl;
+            std::cout << "Target Instruction address: " << std::hex << target_intr << std::endl;
+            PIN_ExitProcess(1);
+        }
     }
 
-    RTN_Close(rtn);
-}
-
-VOID IBT_Instrumentation(ADDRINT* target_intr) {
-
-    ADDRINT instruction;
-    PIN_SafeCopy(&instruction, target_intr, sizeof(ADDRINT));
-    PIN_LockClient();
-    std::cout << "Image: " <<  IMG_Name(IMG_FindByAddress((ADDRINT)target_intr)) << std::endl;
-    PIN_UnlockClient();
 }
 
 /* ======================================================================== */
-/* Instrument every Indirect Branch in the image                            */
+/* Image Loading to determine IBT compatibility.                            */
+/* Function called for every loaded image in the application.               */
 /* ======================================================================== */
-VOID InstrumentImage(IMG img) {
-    // Iterate image sections
+VOID ImageLoad(IMG img, VOID* v) {
+
+    if (IMG_HasProperty(img, IMG_PROPERTY_IBT_ENABLED)) {
+        std::cout << IMG_Name(img) << std::endl;
+    }
+
+    // Iterate over image sections
     for (SEC sec = IMG_SecHead(img); SEC_Valid(sec); sec = SEC_Next(sec)) {
-        // Iterate through routines in sections
+        // Iterate over routines in section
         for (RTN rtn = SEC_RtnHead(sec); RTN_Valid(rtn); rtn = RTN_Next(rtn)) {
-            // Open routine
+            // Open the routine for analysis
             RTN_Open(rtn);
 
-            for (INS ins = RTN_InsHead(rtn); INS_Valid(ins); ins = INS_Next(ins)) {
-                if (INS_IsIndirectControlFlow(ins) && !INS_IsRet(ins) && INS_Opcode(ins) != XED_ICLASS_XEND && INS_Opcode(ins) != XED_ICLASS_XBEGIN) {
-                    //std::cout << "Instruction = " << INS_Disassemble(ins) << "; Address = " << std::hex << INS_Address(ins) << std::endl;
-                    INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)IBT_Instrumentation, IARG_BRANCH_TARGET_ADDR, IARG_END);
+            // Get the first instruction of the routine
+            ADDRINT* first_instr = (ADDRINT*) RTN_Address(rtn);
+            UINT32 instruction;
+            PIN_SafeCopy(&instruction, first_instr, sizeof(UINT32));
+ 
+            // Check if the first instruction is an ENDBR64 instruction
+            if (instruction == ENDBR64) {
+                // Mark routine as IBT compatible
+                COMPATIBLE_RTNS[RTN_Id(rtn)] = true;
+
+                // Instument indirect branched inside of routine
+                for (INS ins = RTN_InsHead(rtn); INS_Valid(ins); ins = INS_Next(ins)) {
+                    // Check if the instruction is an indirect branch 
+                    // that is not a return or an xend or an xbegin instruction
+                    if (INS_IsIndirectControlFlow(ins) && !INS_IsRet(ins) && INS_Opcode(ins) != XED_ICLASS_XEND && INS_Opcode(ins) != XED_ICLASS_XBEGIN) {
+                        // Insert a call to IBT_Instrumentation before the branch instruction
+                        INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)IBT_Instrumentation, IARG_BRANCH_TARGET_ADDR, IARG_END);
+                    }
+
                 }
             }
 
-            // close routine
+            // Close the routine after analysis
             RTN_Close(rtn);
+
         }
     }
 }
 
-
 /* ======================================================================== */
-/* Image Loading to determine IBT compatibility                             */
+/* Clears routines from compatible ibt routines when image is unloaded      */
 /* ======================================================================== */
-VOID ImageLoad(IMG img, VOID* v) {
+VOID ImageUnload(IMG img, VOID* v) {
     // Iterate image sections
     for (SEC sec = IMG_SecHead(img); SEC_Valid(sec); sec = SEC_Next(sec)) {
         // Iterate through routines in sections
         for (RTN rtn = SEC_RtnHead(sec); RTN_Valid(rtn); rtn = RTN_Next(rtn)) {
+
             // Open the RTN.
             RTN_Open(rtn);
- 
-            // Get the first instruction of a routine
-            ADDRINT* first_instr = (ADDRINT*) RTN_Address(rtn);
-            UINT32 instruction32;
-            PIN_SafeCopy(&instruction, first_instr, sizeof(ADDRINT));
- 
-            // Check to see if the first instruction is an endbranch
-            // This can determine if the image is compiled with IBT
-            if (instruction == ENDBR64) {
-                // Close routine before moving to function
-                RTN_Close(rtn);
 
-                // Instrument the Image for IBT
-                InstrumentImage(img);
-                return;
-            }
+            // Remove all compatible routines in section
+            // This needs to be done in case an image gets unloaded and reloaded since IDs change
+            COMPATIBLE_RTNS.erase(RTN_Id(rtn));
 
             // Close the RTN.
-            else {
-                RTN_Close(rtn);
-            }
+            RTN_Close(rtn);
         }
     }
-
+    
 }
 
 /* ===================================================================== */
@@ -131,11 +146,13 @@ VOID SHSTK_PopAndCheck(CONTEXT* ctxt) {
 /* Instrument all call and ret instructions                              */
 /* ===================================================================== */
 VOID InstrumentInstruction(INS ins, VOID* v) {
+    // Check to see if this is a call instruction
     if(INS_IsCall(ins) || INS_IsFarCall(ins)) {
         // Insert SHSTK_Push before a call instruction
         USIZE ins_size = INS_Size(ins);
         INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)SHSTK_Push, IARG_INST_PTR, IARG_UINT32, ins_size, IARG_END);
     }
+    // Check to see if this is a ret instruction
     else if (INS_IsRet(ins) || INS_IsFarRet(ins)){
         // Insert SHSTK_PopAndCheck before a ret instruction
         INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)SHSTK_PopAndCheck, IARG_CONTEXT, IARG_END);
@@ -163,9 +180,14 @@ int main(int argc, char* argv[])
     // Initialize pin
     if (PIN_Init(argc, argv)) return Usage();
  
-    // Register Routine to be called to instrument rtn
+    // Handles image loading
     IMG_AddInstrumentFunction(ImageLoad, 0);
-    // INS_AddInstrumentFunction(InstrumentInstruction, 0);
+
+    // Handles image unloading
+    IMG_AddUnloadFunction(ImageUnload, 0);
+    
+    // Handles Instrumentation of every call and ret insturction
+    INS_AddInstrumentFunction(InstrumentInstruction, 0);
  
     // Start the program, never returns
     PIN_StartProgram();
